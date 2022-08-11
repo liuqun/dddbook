b6956513fb4bf2d91ff7737348cda51e96cee1c3
刘群 <liuqun@22tianbo.com>
2022-08-11 14:12:46 +0800

    bank-types: 优化Currency货币币种的equals()和compareTo()比较方法

--- bank-types/src/main/java/com/example/bank/types/Currency.java
+++ bank-types/src/main/java/com/example/bank/types/Currency.java
@@ -1,13 +1,14 @@
 package com.example.bank.types;
 
+import javax.annotation.Nonnull;
 
-public class Currency {
+public class Currency implements Comparable<Currency> {
 
-    private String value;
+    private final String value;
 
-    public Currency(String value) {
-        if (value == null || "".equals(value)){
-            throw new IllegalArgumentException("货币不能为空");
+    public Currency(@Nonnull String value) {
+        if (org.apache.commons.lang3.StringUtils.isEmpty(value)){
+            throw new IllegalArgumentException("货币币种不允许设置为空字符串");
         }
         this.value = value;
     }
@@ -16,14 +17,36 @@ public class Currency {
         return value;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    /**
+     * 标准 equals() 方法
+     * @param other 另一个货币币种对象
+     * @return 货币币种相同返回true, 反之返回false
+     */
+    public boolean equals(@Nonnull Currency other) {
+        return compareTo(other) == 0;
+    }
+
+    @Override
+    public boolean equals(@Nonnull Object obj) {
+        if (obj.getClass() != Currency.class)
+            return false;
+        return this.equals((Currency) obj);
+    }
+
+    /**
+     * 标准 compareTo() 方法
+     * @param other 另一个货币币种对象
+     * @return 货币币种相同返回0值, 不相同时返回非零整数
+     */
+    @Override
+    public int compareTo(@Nonnull Currency other) {
+        return org.apache.commons.lang3.StringUtils.compare(value, other.value);
     }
 
     @Override
     public String toString() {
-        return "Currency{" +
-                "value='" + value + '\'' +
-                '}';
+        return "Currency{"
+                + "value='" + value
+                + "'}";
     }
 }
